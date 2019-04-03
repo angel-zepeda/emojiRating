81eacdd377212da04bab75ac77c08b0e9fb857cb
angel-zepeda <angel.zepeda0508@hotmail.com>
2019-04-02 20:34:30 -0600

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Events } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  "cal" = {
+   "valor1": "HOla"
+  }
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public events: Events) {
+    events.subscribe('star-rating:changed', (starRating) => {
+      console.log("Cal: "+starRating);
+    });
   }
 
 }
